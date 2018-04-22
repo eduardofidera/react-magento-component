48f0dde714bf773808450c8dc9107fd720c73ab7
Eduardo Souza <eduardofidera@gmail.com>
2018-04-22 20:24:39 -0300

    init magento 1 module

---
+++ app/code/local/Fidera/Component/Block/Index.php
@@ -0,0 +1,4 @@
+<?php
+class Fidera_Component_Block_Index extends Mage_Core_Block_Template
+{
+}
---
+++ app/code/local/Fidera/Component/controllers/IndexController.php
@@ -0,0 +1,9 @@
+<?php
+
+class Fidera_Component_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        die('oi');
+    }
+}
